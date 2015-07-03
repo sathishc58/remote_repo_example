5a7be3fdab5928e2f6e4d25c3022cc0c4102ac6f
root <root@oc3855733574.ibm.com>
2015-07-03 11:26:45 +0530

    Files modified for tag v3

--- four.c
+++ four.c
@@ -1,2 +1,3 @@
 File 4 created to test git hub
 File 4 modified for 2nd tag
+File 4 modified for 3rd tag
--- three.c
+++ three.c
@@ -1,2 +1,3 @@
 File 3 created to test git hub
 File 3 modified for 2nd tag
+File 3 modified for 3rd tag
