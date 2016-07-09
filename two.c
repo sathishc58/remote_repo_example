a59a14d903010354b66e7b3046fca08c72462219
sathishc58 <sathishc58@gmail.com>
2016-07-09 19:39:26 +0530

    Update two.c

--- two.c
+++ two.c
@@ -2,3 +2,4 @@ File 2 created to test git hub
 File 2 modified for 2nd tag
 File 2 modified for git pull from local repository
 File 2 modified for git push from local repository
+File 2 modified for git fetch from local repository
