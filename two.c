88d105f81b4f7f0963c76a9aa9b0603bc452e02b
sathiku1 <sathiku1@in.ibm.com>
2016-07-09 19:52:55 +0530

    After fetch

--- two.c
+++ two.c
@@ -3,3 +3,4 @@ File 2 modified for 2nd tag
 File 2 modified for git pull from local repository
 File 2 modified for git push from local repository
 File 2 modified for git fetch from local repository
+File 2 modified for git push from local repository after git fetch
